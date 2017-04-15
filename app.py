00e366e395c26c5010cc735b936edcc751e82a6c
Пётр Радилов(HomePC) <Illusionist.nemo@gmail.com>
2017-04-15 18:19:44 +0300

    no message

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify, escape, redirect, abort, url_for, make_response
+from pony.orm import *
+from pytz import timezone
+from datetime import datetime
+from hashlib import sha512
+from uuid import uuid4
+from urllib.parse import quote
+
+app = Flask(__name__)
+app.debug = True
+
+db = Database()
