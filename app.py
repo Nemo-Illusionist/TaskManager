51b160c8615eee99d63ba36ac9031ec1093f25a4
Alexander-Lysenko <linjay@ya.ru>
2017-04-18 23:18:56 +0300

    Table Templates

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify, escape, redirect, abort, url_for, make_response
 from pony.orm import *
-from pytz import timezone
 from datetime import datetime
 from hashlib import sha512
 from uuid import uuid4
@@ -10,4 +9,16 @@ app = Flask(__name__)
 app.debug = True
 
 
+@app.route('/')
+def index():
+    return render_template('projects.html')
 
+
+@app.route('/structure')
+def structure():
+    return render_template('structure.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
