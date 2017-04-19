cdfdf696c3848847cfbecdf40720f8c67a1e8d36
Пётр Радилов(WorkPC) <illusionist.nemo@gmail.com>
2017-04-19 17:58:16 +0300

    изменения класса db

--- DataManager/Entity/ParticipantsProject.py
+++
@@ -1,8 +0,0 @@
-from pony.orm import *
-from DataManager.dbManager import db
-
-class ParticipantsProject(db.Entity):
-    _table_ = "ParticipantsProject"
-    Id = PrimaryKey(int, auto=True)
-    UserId = Required(int)
-    ProjectId = Required(int)
